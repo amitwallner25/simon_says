c7dda0e5f73f71ec0b048b6263d5d76e81dc7632
noasimkin <noasimkin23@gmail.com>
2025-03-19 16:22:45 +0200

    sounds working just need to connect the button sounds to the switch

--- app/src/main/java/com/example/saimonsays/SettingsFragment.java
+++ app/src/main/java/com/example/saimonsays/SettingsFragment.java
@@ -135,6 +135,7 @@ public class SettingsFragment extends Fragment {
 
     private void handleUserInput(int buttonNumber, int soundResId) {
         userInput.add(buttonNumber);
+        playSound(soundResId);
 
         if (userInput.get(userInput.size() - 1).equals(pattern.get(userInput.size() - 1))) {
             if (userInput.size() == pattern.size()) {
@@ -156,7 +157,11 @@ public class SettingsFragment extends Fragment {
         }
     }
 
-
+    private void playSound(int resId) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(getActivity(), resId);
+        mediaPlayer.setVolume(100, 100);
+        mediaPlayer.start();
+    }
 
     public void setSimonSaysListener(SimonSaysListener listener) {
         this.listener = listener;
